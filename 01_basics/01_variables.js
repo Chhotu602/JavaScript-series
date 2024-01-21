42d344cc2094ac0fe0ed3ca827771145647e72e3
Eklavya <141182144+Chhotu602@users.noreply.github.com>
2024-01-21 11:23:59 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId=144553
+let accountEmail ="eklavya@gmail.com"
+var accountPassword="12355"
+accountCity="jaipur"
+let accountState;
+//accountId = 2
+accountEmail="ek@gmail.com"
+accountPassword="2345"
+accountCity="Patna"
+console.table([accountEmail, accountId, accountPassword, accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all js code as newer version
+// alert(3+3) we are using node js not browser
+//code readability that should be high
+let name="Eklavya"
+let age= 18
+let isLoggedIn=false
+
+
+//number => 2 to power 53
+//bigint => 
+//string => " " and ' '
+//boolean => true/false
+//null => stand alone value
+//undefined 
+//symbol => unique
+//object 
+console.log(typeof undefined);
+console.log(typeof null);
