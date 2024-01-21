e6e6832e66208137c3cb42cf144a7b969880c5c4
Eklavya <141182144+Chhotu602@users.noreply.github.com>
2024-01-21 11:46:08 +0000

    conversion

---
+++ 01_basics/03_typeconversion.js
@@ -0,0 +1,13 @@
+let age =33
+let score = "33abc"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+//NULL => 0
+//true => 1
