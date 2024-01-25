b2a356cd90f791b1b1c305fc4d55182371868dcb
Eklavya <141182144+Chhotu602@users.noreply.github.com>
2024-01-25 18:36:33 +0000

    arrays

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,25 @@
+const marvel_heros=["thor","iron","spiderman"]
+const dc_heros = ["superman","flash","batman"]
+
+
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// const allHeros=marvel_heros.concat(dc_heros)
+// console.log(allHeros);
+
+// const all_new_heros =[...marvel_heros, ...dc_heros]
+
+// console.log(all_new_heros);
+
+// const another_array=[1,2,3[4,5,6],7,[6,7[4,5]]]
+
+// const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array);
+
+const name=("Eklavya")
+
+console.log(Array.isArray("Eklavya"))
+console.log(Array.from("Eklavya"))
+console.log(Array.from({name: "Eklavya"}))
