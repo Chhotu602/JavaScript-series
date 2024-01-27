963c25f559861ed4503e957c8af694f01bee6ab5
Eklavya <141182144+Chhotu602@users.noreply.github.com>
2024-01-27 11:41:52 +0000

    objects

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -1,8 +1,6 @@
 const marvel_heros=["thor","iron","spiderman"]
 const dc_heros = ["superman","flash","batman"]
 
-
-
 // console.log(marvel_heros);
 // console.log(marvel_heros[3][1]);
 
@@ -13,7 +11,7 @@ const dc_heros = ["superman","flash","batman"]
 
 // console.log(all_new_heros);
 
-const another_array=[1,2,3[4,5,6],7,[6,7[4,5]]]
+const another_array=[1,2,3,[4,5,6],7,[6,7,[4,5]]]
 
 const real_another_array = another_array.flat(Infinity)
 console.log(real_another_array);
@@ -21,5 +19,11 @@ console.log(real_another_array);
 const name=("Eklavya")
 
 console.log(Array.isArray("Eklavya"))
-console.log(Array.from("Eklavya"))
+console.log(Array.from("Eklavy"))
 console.log(Array.from({name: "Eklavya"}))
+
+let score1=100
+let score2=200
+let score3=300
+
+console.log(Array.of(score1,score2,score3));
