8ef31a962edd1fa6d5faa82f8b2ba1ebb751ab49
Eklavya <141182144+Chhotu602@users.noreply.github.com>
2024-02-01 12:59:43 +0000

    arrow

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,51 @@
+const user = {
+    username: "Eklavya",
+    price: 999,
+
+    welcomeMessage: function(){
+        console.log(`${this.username},welcome to website`)
+        console.log(this);
+    }
+
+}
+
+// user.welcomeMessage()
+// user.username ="sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     console.log(this);
+//     console.log(this.username);
+// }
+// chai()
+
+// const chai = function (){
+//     let username = "eklavya"
+//     console.log(this.username);
+// }
+
+// const chai = () => {
+//     let username = "eklavya"
+//     console.log(this);
+// }
+
+// chai()
+
+// const addTwo = (num1,num2) => {
+
+//     return num1+num2
+// }
+
+// const addTwo = (num1,num2) =>  num1+num2
+
+
+
+// const addTwo = (num1,num2) =>  (num1+num2)
+
+
+const addTwo = (num1,num2) =>  ({username:"eklavya"})
+
+
+console.log(addTwo(3,4))
